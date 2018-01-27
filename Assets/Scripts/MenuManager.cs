809c0f92ee3bf97af770846996c3a4442a0417bf
Aecio Jorge <aecio.jorge@gmail.com>
2018-01-27 18:40:34 -0400

    add start screen and ui menu guide

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour {
+
+	public GameObject mainMenu;
+	public GameObject creditsMenu;
+
+	//string current = "menu";
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	} 
+
+	public void InitGame(){
+		//current = "game";
+		//mainMenu.SetActive (false);
+		SceneManager.LoadScene("Main", LoadSceneMode.Single);
+
+	}
+
+	public void showCredits(){
+		//current = "credits";
+		mainMenu.SetActive (false);
+		creditsMenu.SetActive (true);
+	}
+
+	public void showMenu(){
+		//current = "menu";
+		mainMenu.SetActive (true);
+		creditsMenu.SetActive (false);
+	}
+}
