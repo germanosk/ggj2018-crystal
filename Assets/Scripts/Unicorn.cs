19b98d1fa3264b71026555866a29aedd48587ac7
Germano Assis <germanobioinfo@gmail.com>
2018-01-27 19:51:12 -0400

    Adding parameters to animation controller and trail

--- Assets/Scripts/Unicorn.cs
+++ Assets/Scripts/Unicorn.cs
@@ -8,6 +8,9 @@ public class Unicorn : MonoBehaviour
     public float jumpSpeed = 8.0F;
     public float gravity = 20.0F;
     private Vector3 moveDirection = Vector3.zero;
+
+    public Animator animator;
+
     void Update()
     {
         CharacterController controller = GetComponent<CharacterController>();
@@ -16,6 +19,9 @@ public class Unicorn : MonoBehaviour
             moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
             moveDirection = transform.TransformDirection(moveDirection);
             moveDirection *= speed;
+
+            animator.SetFloat("x",- moveDirection.x);
+            animator.SetFloat("y", moveDirection.z);
         }
         moveDirection.y -= gravity * Time.deltaTime;
         controller.Move(moveDirection * Time.deltaTime);
